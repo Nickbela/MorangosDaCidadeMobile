378a938414df93de735f3c2c2692912aec3b4835
Nickbela <nicolecastroferraz@gmail.com>
2024-10-18 19:32:27 -0300

    Adicionado validação de email via regex

--- app/src/main/java/RegrasDeNegocio/Sistema.java
+++ app/src/main/java/RegrasDeNegocio/Sistema.java
@@ -2,6 +2,8 @@ package RegrasDeNegocio;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sistema {
     private List<Cliente> clientes;
@@ -26,5 +28,26 @@ public class Sistema {
         System.out.println("Identificador (CPF ou email) ou senha inválidos.");
         return null;
     }
+
+    private boolean isEmailValido(String email) {
+        // Expressão regular para validar emails comuns
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    /*Necessário adicionar esse método no botão de cadastro no front de form separada:
+    * sugestão:
+    *
+    * if (sistema.isEmailValido(email)) {
+        sistema.cadastrarCliente(novoCliente);
+        Toast.makeText(this, "Cliente cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+    } else {
+        Toast.makeText(this, "Erro: Email inválido", Toast.LENGTH_SHORT).show();
+    }
+    *
+    * mas claro no lugar dos toasts talvez colocar a mudança na caixa de texto a baixo do
+    * campo de entrada de email para indicar erro.*/
 }
 
